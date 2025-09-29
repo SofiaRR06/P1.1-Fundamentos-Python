da40633a5d893dfc14f742c736829088cd3a4492
SofiaRR06 <alumno.708095@ies-azarquiel.es>
2025-09-29 12:13:39 +0200

    Pedir datos al usuario

---
+++ ejercicio1_tipos_datos_y_entrada_de_usuario.py
@@ -0,0 +1,6 @@
+#Pedimos datos al usuario
+nombre = str(input("Introduce tu nombre: "))
+edad = int(input("Introduce tu edad: "))
+altura = float(input("Introduce tu edad en metros: "))
+
+#Mostramos el texto de ejemplo
